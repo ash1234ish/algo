c60da5997100ffaa4d268ef10dcabca907d13a7f
ash1234ish <inbox.ashishagrawal@yahoo.com>
2020-01-18 01:15:04 +0530

    added few more programs

--- Algos/src/com/practice/String/AllPossibleSubStrings.java
+++ Algos/src/com/practice/String/AllPossibleSubStrings.java
@@ -1,19 +1,45 @@
 package com.practice.String;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AllPossibleSubStrings {
 
 	public static void main(String[] args) {
-		printSubStrings("pwwkew");
-
+		System.out.println(lengthOfLongestSubstring("pwwkew"));
 	}
-	
-	public static void printSubStrings(String str) {
-		for(int i = 0; i < str.length()-1 ; i++) {
-			String c = str.substring(i,i+1);
-			for(int j = i+1 ; j < str.length() ; j++) {
-				System.out.println(c+str.substring(j,str.length()));
+
+	public static int lengthOfLongestSubstring(String s) {
+		if(s == null || s.isEmpty())
+			return 0;
+		else{
+			int largestSofar = 1;
+			for(int i = 0; i < s.length()-1 ; i++) {
+				String c = s.substring(i,i+1);
+				for(int j = 0 ; j < s.length() -i ; j++) {
+					String temp = c+s.substring(i+1,s.length()-j);
+					int uniqueLength = 0;
+					if(uniqueStringlength(temp)) {
+						uniqueLength = temp.length();
+					}
+					largestSofar = (largestSofar > uniqueLength) ? largestSofar : uniqueLength;
+				}
+				largestSofar = (largestSofar > c.length()) ? largestSofar : c.length();
 			}
-			System.out.println(c);
+			return largestSofar;
+		}    
+	}
+
+	public static boolean uniqueStringlength(String str) {
+		Set<String> set = new HashSet<String>();
+		for(int i = 0 ; i < str.length() ; i++) {
+			String c = str.substring(i,i+1);
+			if(set.contains(c)) {
+				return false;
+			}else {
+				set.add(c);
+			}
 		}
+		return true;
 	}
 }
