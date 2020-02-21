46adcedee2f3a86bd18dee0966ba3b5a11d49452
ash1234ish <inbox.ashishagrawal@yahoo.com>
2020-02-21 21:59:31 +0530

    Added few programs

---
+++ Algos/src/com/practice/arrays/SearchForRange.java
@@ -0,0 +1,39 @@
+package com.practice.arrays;
+
+import java.util.Arrays;
+
+public class SearchForRange {
+	
+	public static void main(String[] args) {
+		
+		int [] arr= new int [] {5,7,7,8,8,10};
+		Arrays.stream(searchRange(arr,1)).forEach(System.out::println);
+	}
+	
+	public static int[] searchRange(int []arr, int target) {
+		
+		if(arr == null || arr.length == 0)
+			return new int [] {-1,-1};
+		
+		int low =0 ,high = arr.length -1;
+		
+		while(low < high) {
+			int mid = (low+high)/2;
+			
+			if(arr[mid] == target) {
+				int i = mid , j = mid;
+				while(arr[i]==target) {i--;}
+				while(arr[j]== target){j++;}
+				return new int[] {i+1,j-1};
+			}
+			if(target < arr[mid]) {
+				high = mid;
+			}
+			else {
+				low = mid+1;
+			}
+		}
+		return new int [] {-1,-1};
+	}
+
+}
